ae125e99925a46896b957636d3845d2e617e236b
Tobias Watzek <spitzfaust@gmail.com>
2020-12-18 18:19:43 +0100

    Add solution for day 14

--- Days/Day13.cs
+++ Days/Day13.cs
@@ -9,23 +9,47 @@ namespace adventofcode2020.Days
     public class Day13 : IDay
     {
         public int Number { get; } = 13;
+
         public async Task<(string firstSolution, string secondSolution)> Solve()
         {
-            var (timestamp, ids) = await ParseInputForFirst();
-            var firstSolution = SolveFirst(timestamp, ids);
-
-            return (firstSolution, "");
+            var firstSolution = await SolveFirst();
+            var secondSolution = await SolveSecond();
+            
+            return (firstSolution, secondSolution);
         }
 
-        private static string SolveFirst(int timestamp, IEnumerable<int> ids)
+        private static async Task<string> SolveFirst()
         {
-            var (id, nextDeparture) = ids.Select(id => (id, nextDeparture: (int)(Math.Ceiling(timestamp / (double)id) * id)))
+            var (timestamp, ids) = await ParseInputForFirst();
+
+            var (id, nextDeparture) = ids
+                .Select(id => (id, nextDeparture: (int) (Math.Ceiling(timestamp / (double) id) * id)))
                 .OrderBy(t => t.nextDeparture)
                 .First();
 
             var minutesToWait = nextDeparture - timestamp;
 
             return (id * minutesToWait).ToString();
+        }
+
+        private static async Task<string> SolveSecond()
+        {
+            var idsWithOffset = await ParseInputForSecond();
+
+            return "";
+        }
+
+
+        private static async Task<IEnumerable<(int id, int offset)>> ParseInputForSecond()
+        {
+            var lines = await File.ReadAllLinesAsync("./input/day13.txt");
+            var idsWithOffset = lines[1]
+                .Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Select((c, i) => c == "x" ? (id: -1, offset: i) : (id: int.Parse(c), offset: i))
+                .Where(t => t.id != -1)
+                .ToArray();
+
+            return idsWithOffset;
         }
 
         private static async Task<(int timestamp, int[] ids)> ParseInputForFirst()
